75437d3992dd3f98a8b16e2fd4ffbb7c75614e56
Vale <valerauko@gmail.com>
2020-11-21 18:56:41 +0900

    Fix project.clj license

--- api/project.clj
+++ api/project.clj
@@ -1,8 +1,8 @@
-(defproject karasawa "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject karasawa "0.0.1"
+  :description "Experimental blog engine"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "AGPL-3.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.gnu.org/licenses/agpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot karasawa.core
   :target-path "target/%s"
